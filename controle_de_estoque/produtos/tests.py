94a9ce5722c8f60f3cc931845c9ef0cb1b517415
diegoreiss <diegoportal.reis@outlook.com>
2023-09-19 00:53:19 -0300

    testcase para criar produto com tamanho

--- controle_de_estoque/produtos/tests.py
+++ controle_de_estoque/produtos/tests.py
@@ -1,56 +1,19 @@
-from decimal import Decimal
-
 from django.test import TestCase
 from django.db import transaction
-from django.core.exceptions import ValidationError
 
-from .models import Categoria, Produto
-from .service import ProdutoService
+from .models import Tamanho, Produto, ProdutoTamanho
 
 # Create your tests here.
 
-class ProdutoTestCase(TestCase):
 
+class ProdutoTamanhoTestCase(TestCase):
     @transaction.atomic
-    def test_criar_produto_com_sucesso(self):
-        campos_categoria = {
-            "nome": "Inverno"
-        }
+    def test_criar_produto_com_tamanho_com_sucesso(self):
+        tamanho = Tamanho.objects.get(id=1)
+        produto = Produto.objects.create(nome="Casaco Rosa", descricao="Um lindo casaco")
+        produto_tamanho = ProdutoTamanho.objects.create(produto=produto, tamanho=tamanho, quantidade=10, codigo_de_barras=9537238)
+        produto_tamanho_test = ProdutoTamanho.objects.get(id=produto_tamanho.id)
 
-        campos_produto = {
-            "nome": "Casaco",
-            "tamanho": "PP",
-            "preco_de_compra": Decimal("5.10"),
-        }
-
-        categoria_test = Categoria(**campos_categoria)
-        produto_test = Produto(**campos_produto)
-
-        ProdutoService.salvar_produto(categoria_test, produto_test)
-
-        categoria_salva = Categoria.objects.get(id=categoria_test.id)
-        produto_salvo = Produto.objects.get(id=produto_test.id)
-
-
-        self.assertEqual(categoria_test, categoria_salva)
-        self.assertEqual(produto_test, produto_salvo)
-        self.assertEqual(categoria_salva.id, produto_salvo.categoria_id)
-
-    def test_criar_produto_com_preco_de_compra_negativo_ou_nulo(self):
-        campos_categoria = {
-            "nome": "Inverno"
-        }
-        campos_produto = {
-            "nome": "Casaco",
-            "tamanho": "PP",
-            "preco_de_compra": Decimal("-5.10"),
-        }
+        self.assertEqual(tamanho.id, produto_tamanho_test.tamanho.id)
+        self.assertEqual(produto.id, produto_tamanho_test.produto.id)
         
-        categoria_test = Categoria(**campos_categoria)
-        produto_test = Produto(**campos_produto)
-
-        with self.assertRaises(ValidationError) as test_ex:
-            ProdutoService.salvar_produto(categoria_test, produto_test)
-
-        self.assertEqual(str(test_ex.exception.message), f"{produto_test.preco_de_compra} não é um valor válido!")
-
