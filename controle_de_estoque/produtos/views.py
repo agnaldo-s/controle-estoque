e3049cbe33aae894e351fd1cd628a2babdacf3be
diegoreiss <diegoportal.reis@outlook.com>
2023-09-28 09:00:11 -0300

    func produto

--- controle_de_estoque/produtos/views.py
+++ controle_de_estoque/produtos/views.py
@@ -1,9 +1,12 @@
+import json
+
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.http import JsonResponse
+from django.db import transaction
 from http import HTTPStatus
 
-from .models import Produto
+from .models import Produto, Tamanho, ProdutoTamanho
 
 # Create your views here.
 
@@ -17,6 +20,7 @@ def relatorio(request):
         numero_da_pagina = 1
     
     paginator_produtos = Paginator(Produto.objects.all(), OFFSET)
+    tamanhos = Tamanho.objects.all()
 
     if nome_like:
         paginator_produtos = Paginator(Produto.objects.filter(nome__contains=nome_like), OFFSET)
@@ -24,9 +28,25 @@ def relatorio(request):
     paginator_elided = list(paginator_produtos.get_elided_page_range(number=numero_da_pagina, on_each_side=3, on_ends=1))
     pagina = paginator_produtos.get_page(numero_da_pagina)
 
-    return render(request, "produtos/relatorio.html", context={"pagina": pagina, "paginator": paginator_produtos, "elided": paginator_elided, "nome_like": nome_like})
+    return render(request, "produtos/relatorio.html", 
+                  context={
+                      "pagina": pagina, 
+                      "paginator": paginator_produtos, 
+                      "elided": paginator_elided, 
+                      "nome_like": nome_like, 
+                      "tamanhos": tamanhos
+                    }
+                  )
 
 
+@transaction.atomic
 def me(request):
-    print(request.POST)
+    produto_request = json.loads(request.POST["produto"])
+    produto = Produto.objects.create(nome=produto_request["nome"], descricao=produto_request["descricao"])
+
+    for tamanho_selecionado in produto_request["tamanhos"]:
+        tamanho = Tamanho.objects.get(nome=tamanho_selecionado["tamanho"])
+        ProdutoTamanho.objects.create(produto=produto, tamanho=tamanho, quantidade=tamanho_selecionado["quantidade"],
+                                        codigo_de_barras=produto_request["codigo_de_barras"])
+
     return JsonResponse({}, status=HTTPStatus.CREATED)
