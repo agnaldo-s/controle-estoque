3affba3369e27fb508b609a8a2bdd1ae738fbac0
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-10-22 00:00:11 -0300

    commit

--- controle_de_estoque/core/urls.py
+++ controle_de_estoque/core/urls.py
@@ -20,4 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("produtos.urls"))
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
