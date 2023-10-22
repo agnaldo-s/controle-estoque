6a9a4532fa71e186d988a908fb3d46622313253d
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-10-22 00:05:00 -0300

    commit

--- controle_de_estoque/produtos/urls.py
+++ controle_de_estoque/produtos/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("listar/", views.listar_produtos, name="listar_produtos"),
     path("tamanhos/listar/", views.listar_tamanhos, name="listar_tamanhos"),
     path("desativar/<int:pk>", views.desativar_produto, name="desativar_produto")
+    path('product_detail/<int:id>', views.product_detail, name='product_detail')
 ]
