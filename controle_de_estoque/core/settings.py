53f1ec0258417e22d517ad177da4086ba2ce1331
chrystian-souza <chrystian.souza.silveira@gmail.com>
2023-10-22 00:01:37 -0300

    commit

--- controle_de_estoque/core/settings.py
+++ controle_de_estoque/core/settings.py
@@ -116,10 +116,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'templates/static')
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
